8590ef79d3e4c310d57ad48011741c3abfd330ab
Past <pastnrxe@gmail.com>
2020-06-08 16:18:06 +0300

    Добавил весь код

---
+++ BlackJack/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace BlackJack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.dealButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Banker";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(12, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click_1);
+            // 
+            // dealButton
+            // 
+            this.dealButton.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dealButton.Location = new System.Drawing.Point(12, 327);
+            this.dealButton.Name = "dealButton";
+            this.dealButton.Size = new System.Drawing.Size(75, 23);
+            this.dealButton.TabIndex = 6;
+            this.dealButton.Text = "Deal";
+            this.dealButton.UseVisualStyleBackColor = true;
+            this.dealButton.Click += new System.EventHandler(this.dealButton_Click_1);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.Location = new System.Drawing.Point(12, 356);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resetButton.Location = new System.Drawing.Point(12, 41);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 8;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 298);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 26);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Player ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(107, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(80, 110);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(107, 269);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 110);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(199, 269);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 110);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(169, 172);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(18, 26);
+            this.resultLabel.TabIndex = 13;
+            this.resultLabel.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(414, 391);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dealButton);
+            this.Controls.Add(this.stopButton);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BlackJack";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button dealButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ BlackJack/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    public partial class Form1 : Form
+    {
+        List<Card> deck = new List<Card>()
+        {
+            #region spades
+            new Card() { Value  = 2, Name = "Two Spades", Image = @"..\Image\2S.png" },
+            new Card() { Value = 3, Name = "Three Spades", Image = @"..\Image\3S.png"},
+            new Card() { Value = 4, Name =  "Four Spades", Image = @"..\Image\4S.png"},
+            new Card() { Value = 5, Name = "Five Spades", Image = @"..\Image\5S.png" },
+            new Card() { Value = 6, Name = "Six Spades", Image = @"..\Image\6S.png" },
+            new Card() { Value = 7, Name = "Seven Spades", Image = @"..\Image\7S.png" },
+            new Card() { Value = 8, Name = "Eight Spades", Image = @"..\Image\8S.png" },
+            new Card() { Value = 9, Name = "Nine Spades", Image = @"..\Image\9S.png" },
+            new Card() { Value = 10, Name = "Ten Spades", Image = @"..\Image\10S.png" },
+            new Card() { Value = 10, Name = "Jack Spades", Image = @"..\Image\JS.png" },
+            new Card() { Value = 10, Name = "Queen Spades", Image = @"..\Image\QS.png" },
+            new Card(){ Value = 10, Name = "King Spades", Image = @"..\Image\KS.png" },
+            new Card(){ Value = 11, Name = "Ace Spades", Image = @"..\Image\AS.png" },
+            #endregion
+            #region diamonds
+            new Card() { Value  = 2, Name = "Two Diamonds", Image = @"..\Image\2D.png" },
+            new Card() { Value = 3, Name = "Three Diamonds", Image = @"..\Image\3D.png" },
+            new Card() { Value = 4, Name =  "Four Diamonds", Image = @"..\Image\4D.png"},
+            new Card() { Value = 5, Name = "Five Diamonds", Image = @"..\Image\5D.png" },
+            new Card() { Value = 6, Name = "Six Diamonds", Image = @"..\Image\6D.png" },
+            new Card(){ Value = 7, Name = "Seven Diamonds", Image = @"..\Image\7D.png" },
+            new Card() { Value = 8, Name = "Eight Diamonds", Image = @"..\Image\8D.png" },
+            new Card() { Value = 9, Name = "Nine Diamonds", Image = @"..\Image\9D.png" },
+            new Card() { Value = 10, Name = "Ten Diamonds", Image = @"..\Image\10D.png" },
+            new Card() { Value = 10, Name = "Jack Diamonds", Image = @"..\Image\JD.png" },
+            new Card() { Value = 10, Name = "Queen Diamonds", Image = @"..\Image\QD.png" },
+            new Card(){ Value = 10, Name = "King Diamonds", Image = @"..\Image\KD.png" },
+            new Card(){ Value = 11, Name = "Ace Diamonds", Image = @"..\Image\AD.png" },
+            #endregion
+            #region clubs
+            new Card() { Value  = 2, Name = "Two Clubs", Image = @"..\Image\2C.png" },
+            new Card() { Value = 3, Name = "Three Clubs", Image = @"..\Image\3C.png" },
+            new Card() { Value = 4, Name =  "Four Clubs", Image = @"..\Image\4C.png"},
+            new Card() { Value = 5, Name = "Five Clubs", Image = @"..\Image\5C.png" },
+            new Card() { Value = 6, Name = "Six Clubs", Image = @"..\Image\6C.png" },
+            new Card(){ Value = 7, Name = "Seven Clubs", Image = @"..\Image\7C.png" },
+            new Card() { Value = 8, Name = "Eight Clubs", Image = @"..\Image\8C.png" },
+            new Card() { Value = 9, Name = "Nine Clubs", Image= @"..\Image\9C.png" },
+            new Card() { Value = 10, Name = "Ten Clubs", Image = @"..\Image\10C.png" },
+            new Card() { Value = 10, Name = "Jack Clubs", Image = @"..\Image\JC.png" },
+            new Card() { Value = 10, Name = "Queen Clubs", Image = @"..\Image\QC.png" },
+            new Card(){ Value = 10, Name = "King Clubs", Image = @"..\Image\KC.png" },
+            new Card(){ Value = 11, Name = "Ace Clubs", Image = @"..\Image\AC.png" },
+            #endregion
+            #region hearts
+            new Card() { Value  = 2, Name = "Two Hearts", Image = @"..\Image\2H.png" },
+            new Card() { Value = 3, Name = "Three Hearts", Image = @"..\Image\3H.png" },
+            new Card() { Value = 4, Name =  "Four Hearts", Image = @"..\Image\4H.png"},
+            new Card() { Value = 5, Name = "Five Hearts", Image = @"..\Image\5H.png" },
+            new Card() { Value = 6, Name = "Six Hearts", Image = @"..\Image\6H.png" },
+            new Card(){ Value = 7, Name = "Seven Hearts", Image = @"..\Image\7H.png" },
+            new Card() { Value = 8, Name = "Eight Hearts", Image = @"..\Image\8H.png" },
+            new Card() { Value = 9, Name = "Nine Hearts", Image = @"..\Image\9H.png" },
+            new Card() { Value = 10, Name = "Ten Hearts", Image = @"..\Image\10H.png" },
+            new Card() { Value = 10, Name = "Jack Hearts", Image = @"..\Image\JH.png" },
+            new Card() { Value = 10, Name = "Queen Hearts", Image = @"..\Image\QH.png" },
+            new Card(){ Value = 10, Name = "King Hearts", Image = @"..\Image\KH.png" },
+            new Card(){ Value = 11, Name = "Ace Hearts", Image = @"..\Image\AH.png" }
+            #endregion
+        };
+        List<Card> playercardList = new List<Card>()
+        {
+            new Card() { Value  = 0, Name = "null", Image = "null" }
+        };
+        List<Card> bankercardList = new List<Card>()
+        {
+            new Card() { Value  = 0, Name = "null", Image = "null" }
+        };
+        int playercardSum = 0;
+        int bankercardSum = 0;
+        Random random = new Random();
+        List<int> usedCards = new List<int>();
+        List<PictureBox> bankerbox = new List<PictureBox>();
+        List<PictureBox> playerbox = new List<PictureBox>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetGame();
+        }
+        private int selectRandomCard()
+        {
+            int randomCard;
+            randomCard = random.Next(0, deck.Count);
+            return randomCard;
+        }
+        private void sumPlayerCards()
+        {
+            playercardSum = 0;
+            for (int i = 0; i < playercardList.Count; i++)
+            {
+                playercardSum += playercardList[i].Value;
+            }
+
+            if (playercardSum > 21)
+            {
+                foreach (Card c in playercardList)
+                {
+                    if (c.Value == 11)
+                    {
+                        playercardSum -= 10;
+                        if (playercardSum <= 21)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        private void sumBankerCards()
+        {
+            bankercardSum = 0;
+            for (int i = 0; i < bankercardList.Count; i++)
+            {
+                bankercardSum += bankercardList[i].Value;
+            }
+            if (bankercardSum > 21)
+            {
+                foreach (Card c in bankercardList)
+                {
+                    if (c.Value == 11)
+                    {
+                        bankercardSum -= 10;
+                        if (bankercardSum <= 21)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        private void resetGame()
+        {
+            resultLabel.Text = null;
+            displayCardBack(pictureBox1);
+            displayCardBack(pictureBox2);
+            displayCardBack(pictureBox4);
+            foreach (PictureBox pb in playerbox)
+            {
+                this.Controls.Remove(pb);
+            }
+            playerbox = new List<PictureBox>();
+            foreach (PictureBox pb in bankerbox)
+            {
+                this.Controls.Remove(pb);
+            }
+            bankerbox = new List<PictureBox>();
+            playercardSum = 0;
+            bankercardSum = 0;
+            playercardList.Clear();
+            bankercardList.Clear();
+            usedCards.Clear();
+            resultLabel.Text = "Player choice";
+        }
+        private void displayCardBack(PictureBox picturebox)
+        {
+            picturebox.ImageLocation = @"..\Image\b1fv.png";
+            picturebox.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+        private void pnlTable_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void startButton_Click_1(object sender, EventArgs e)
+        {
+            if (playercardSum > 0)
+            {
+                resultLabel.Text = String.Format
+                ("Already started");
+            }
+
+            else
+            {
+                playercardSum = 0;
+                bankercardSum = 0;
+                #region init player
+                int randomCard1 = selectRandomCard();
+                Card card1 = deck[randomCard1];
+                usedCards.Add(randomCard1);
+                int randomCard2 = selectRandomCard();
+                while (usedCards.Contains(randomCard2))
+                {
+                    randomCard2 = selectRandomCard();
+                }
+                randomCard2 = 1 * randomCard2;
+
+                Card card2 = deck[randomCard2];
+                usedCards.Add(randomCard2);
+
+                playercardList.Add(card1);
+                playercardList.Add(card2);
+
+                pictureBox1.ImageLocation = card1.Image;
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+                pictureBox2.ImageLocation = card2.Image;
+                pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+                #endregion
+                #region init banker
+                int randomCard3 = selectRandomCard();
+                while (usedCards.Contains(randomCard3))
+                {
+                    randomCard3 = selectRandomCard();
+                }
+                randomCard3 = 1 * randomCard3;
+                Card card3 = deck[randomCard3];
+                usedCards.Add(randomCard3);
+
+                bankercardList.Add(card3);
+
+                pictureBox4.ImageLocation = card3.Image;
+                pictureBox4.SizeMode = PictureBoxSizeMode.AutoSize;
+
+                #endregion
+                sumPlayerCards();
+                if (playercardSum == 21)
+                {
+                    resultLabel.Text = String.Format("The sum of your cards is {0}", playercardSum);
+                    MessageBox.Show("You win!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    resetGame();
+                }
+            }
+        }
+        private void resetButton_Click_1(object sender, EventArgs e)
+        {
+            resetGame();
+        }
+        private void dealButton_Click_1(object sender, EventArgs e)
+        {
+            if (playercardSum == 0)
+            {
+                resultLabel.Text = "Press Start";
+            }
+            else
+            {
+                if (playercardSum > 40)
+                {
+                    resetGame();
+                    resultLabel.Text = "Restarting";
+                }
+                else
+                {
+                    playercardSum = 0;
+                    int randomCard = selectRandomCard();
+                    Card card = deck[randomCard];
+                    usedCards.Add(randomCard);
+                    if (usedCards.Contains(randomCard)) randomCard = selectRandomCard();
+                    else randomCard = 1 * randomCard;
+                    PictureBox p3 = new PictureBox();
+                    p3.Width = 71;
+                    p3.Height = 96;
+                    p3.Location = new Point(pictureBox2.Left + 90, 269);
+                    p3.ImageLocation = card.Image;
+                    p3.SizeMode = PictureBoxSizeMode.AutoSize;
+                    this.Controls.Add(p3);
+                    playerbox.Add(p3);
+                    playercardList.Add(card);
+                    sumPlayerCards();
+                    if (playercardSum > 21)
+                    {
+                        resultLabel.Text = String.Format("The sum of your cards is {0}", playercardSum);
+                        MessageBox.Show("You lose!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        resetGame();
+                    }
+                    else if (playercardSum == 21)
+                    {
+                        resultLabel.Text = String.Format
+                            ("The sum of your cards is: {0}, you win!", playercardSum);
+                        MessageBox.Show("You win!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        resetGame();
+                    }
+                }
+            }
+        }
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (playercardSum == 0)
+            {
+                resultLabel.Text = "Press Start";
+                return;
+            }
+            sumBankerCards();
+            while (bankercardSum <= 16)
+            {
+                int randomCard = selectRandomCard();
+                Card card = deck[randomCard];
+                usedCards.Add(randomCard);
+                if (usedCards.Contains(randomCard)) randomCard = selectRandomCard();
+                else randomCard = 1 * randomCard;
+                PictureBox p4 = new PictureBox();
+                p4.Width = 71;
+                p4.Height = 96;
+                p4.Location = new Point(154 + bankerbox.Count * 77, 12);
+                p4.ImageLocation = card.Image;
+                p4.SizeMode = PictureBoxSizeMode.AutoSize;
+                this.Controls.Add(p4);
+                bankerbox.Add(p4);
+                bankercardList.Add(card);
+                sumBankerCards();
+            }
+            if (bankercardSum > 21)
+            {
+                resultLabel.Text = String.Format("The sum of banker cards is {0}", bankercardSum);
+                MessageBox.Show("You win!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                resetGame();
+            }
+            else if (playercardSum <= bankercardSum)
+            {
+                resultLabel.Text = String.Format("The sum of your cards is {0}", playercardSum);
+                MessageBox.Show("You lose!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                resetGame();
+            }
+            else
+            {
+                resultLabel.Text = String.Format("The sum of your cards is {0}", playercardSum);
+                MessageBox.Show("You win!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                resetGame();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    } 
+}
